1e3002d2a28820d655342e2a74244744c0997377
20-ist3AlexseyEremin <78901825+20-ist3AlexseyEremin@users.noreply.github.com>
2021-03-01 19:23:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,13 @@
 // Copyright 2021 NNTU-CS
 
-int gcd(int a, int b) {
-    int res;
-      while ((a != 0) && (b != 0)){ 
-         if (a > b)
-            a -= b;
-        else
-          b -= a;
-      }
-       res = a + b;
-     return res;
-}
+int gcd(int a, int b){ 
+                int res;
+        while ((a != 0) && (b != 0)){ 
+                if (a > b)
+        a -= b;
+                else
+        b -= a;
+        }
+        res = a + b;
+                return res;
+        } 
